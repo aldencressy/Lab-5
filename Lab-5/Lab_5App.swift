b559af7173d9688dfd9f7bd69c011258080f1127
aldencressy <acressy@smu.edu>
2024-11-18 20:18:55 -0600

    Initial Commit

---
+++ Lab-5/Lab_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab_5App.swift
+//  Lab-5
+//
+//  Created by Alden Cressy on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
